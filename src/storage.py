5d26e163a0b73c7dc7017642b99ce2e56a3458c0
Parham Alvani <parham.alvani@gmail.com>
2015-05-26 09:57:21 +0430

    Storage object is added.

--- src/storage.py
+++ src/storage.py
@@ -7,3 +7,28 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 __author__ = 'Parham Alvani'
+
+import os
+
+
+class FileStorage:
+    def __init__(self, folders: list):
+        self.folders = folders
+
+    def getFilesPath(self) -> list:
+        pass
+
+    def getFilesName(self) -> list:
+        retval = []
+        for folder in self.folders:
+            for file in os.listdir(folder):
+                if os.path.isfile(file):
+                    retval.append(os.path.basename(file))
+        return retval
+
+    def getFile(self, mame: str) -> str:
+        pass
+
+# Just for test :-)
+if __name__ == '__main__':
+    print(FileStorage(["."]).getFilesName())
