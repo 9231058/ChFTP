cf67341ce53c00e35b07557fbb8d7447a954d0d9
Parham Alvani <parham.alvani@gmail.com>
2015-05-29 01:26:56 +0430

    GUI improved.

--- src/gui.py
+++ src/gui.py
@@ -16,14 +16,27 @@ class ChFTP(Gtk.Window):
         super(ChFTP, self).__init__(title="ChFTP")
         self.set_border_width(10)
 
+        self.peer_model = Gtk.ListStore(str, str, str)
+        self.peer_view = Gtk.TreeView(self.peer_model)
+
         hbox = Gtk.Box(spacing=6)
         self.add(hbox)
 
-        run_button = Gtk.Button(label="Run")
-        hbox.add(run_button)
+        self.run_button = Gtk.Button(label="Run")
+        self.run_button.connect('clicked', self.on_run_clicked)
+        hbox.add(self.run_button)
+        hbox.pack_start(self.run_button, True, True, 0)
 
-        get_button = Gtk.Button(label="Get", sensitive=0)
-        hbox.add(get_button)
+        self.get_button = Gtk.Button(label="Get", sensitive=0)
+        hbox.add(self.get_button)
+        hbox.pack_start(self.get_button, True, True, 0)
+
+        self.username_entry = Gtk.Entry()
+        self.username_entry.set_text("Username")
+        hbox.pack_start(self.username_entry, True, True, 0)
+
+    def on_run_clicked(self, button):
+        pass
 
 
 win = ChFTP()
