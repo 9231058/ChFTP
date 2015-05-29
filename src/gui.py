539d89414cfb1127b75c66d19edeb1bf61f8a7ff
Parham Alvani <parham.alvani@gmail.com>
2015-05-29 21:22:27 +0430

    GUI improved.

--- src/gui.py
+++ src/gui.py
@@ -13,13 +13,24 @@ from gi.repository import GObject
 import getpass
 import logging
 
+from presence import PresenceService
+from peer import PeerList
+from storage import FileStorage
+from file_transfer import FileTransferServer
+from file_transfer import recv_file
+
 
 class ChFTP(Gtk.Window):
     def __init__(self):
         super(ChFTP, self).__init__(title="ChFTP")
         self.set_border_width(10)
 
+        self.presenceService = None
+        self.fileTransferServer = None
+
         self.peer_model = Gtk.ListStore(str, str, str)
+
+        self.connect("delete-event", self.on_quit)
 
         hbox = Gtk.Box(spacing=6)
         self.add(hbox)
@@ -49,14 +60,22 @@ class ChFTP(Gtk.Window):
 
     def on_run_clicked(self, button):
         username = self.username_entry.get_text()
-        print(username)
-        self.peer_model.clear()
+        self.username_entry.set_sensitive(False)
         self.peer_view.get_column(2).set_title("Remote Filename")
         self.run_button.set_sensitive(False)
         self.add_button.set_sensitive(False)
         self.get_button.set_sensitive(True)
 
-        GObject.timeout_add(200, self.peer_list_update)
+        folders = []
+        for row in self.peer_model:
+            folders.append(row[2])
+
+        self.presenceService = PresenceService(FileStorage(folders).get_files_name(), username)
+        self.presenceService.start()
+        self.fileTransferServer = FileTransferServer()
+        self.fileTransferServer.start()
+
+        GObject.timeout_add(5000, self.peer_list_update)
 
     def on_get_clicked(self, button):
         pass
@@ -72,12 +91,20 @@ class ChFTP(Gtk.Window):
 
         dialog.destroy()
 
+    def on_quit(self, *args):
+        if self.presenceService:
+            self.presenceService.shutdown()
+        Gtk.main_quit(args)
+
     def peer_list_update(self):
-        pass
+        self.peer_model.clear()
+        for peer in PeerList():
+            for file in peer.files:
+                self.peer_model.append([peer.username, peer.ip, file])
+        return True
 
 
 logging.basicConfig(filename="ChFTP.log", level=logging.INFO)
 win = ChFTP()
-win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
