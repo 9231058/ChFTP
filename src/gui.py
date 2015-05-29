821f75444bcfa596168a113597c9e72b675e75f8
Parham Alvani <parham.alvani@gmail.com>
2015-05-29 20:41:21 +0430

    GUI improved.

--- src/gui.py
+++ src/gui.py
@@ -9,7 +9,9 @@
 __author__ = 'Parham Alvani'
 
 from gi.repository import Gtk
+from gi.repository import GObject
 import getpass
+import logging
 
 
 class ChFTP(Gtk.Window):
@@ -18,10 +20,16 @@ class ChFTP(Gtk.Window):
         self.set_border_width(10)
 
         self.peer_model = Gtk.ListStore(str, str, str)
-        self.peer_view = Gtk.TreeView(self.peer_model)
 
         hbox = Gtk.Box(spacing=6)
         self.add(hbox)
+
+        self.peer_view = Gtk.TreeView(self.peer_model)
+        for column_index, column_title in enumerate(["Peer Username", "Peer IP Address", "Local Folder Name"]):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=column_index)
+            self.peer_view.append_column(column)
+        hbox.pack_start(self.peer_view, True, True, 0)
 
         self.add_button = Gtk.Button(label="Add")
         self.add_button.connect('clicked', self.on_add_clicked)
@@ -42,9 +50,13 @@ class ChFTP(Gtk.Window):
     def on_run_clicked(self, button):
         username = self.username_entry.get_text()
         print(username)
+        self.peer_model.clear()
+        self.peer_view.get_column(2).set_title("Remote Filename")
         self.run_button.set_sensitive(False)
         self.add_button.set_sensitive(False)
         self.get_button.set_sensitive(True)
+
+        GObject.timeout_add(200, self.peer_list_update)
 
     def on_get_clicked(self, button):
         pass
@@ -56,11 +68,15 @@ class ChFTP(Gtk.Window):
 
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
-            print("Folder selected: " + dialog.get_filename())
+            self.peer_model.append(["You", "localhost", dialog.get_filename()])
 
         dialog.destroy()
 
+    def peer_list_update(self):
+        pass
 
+
+logging.basicConfig(filename="ChFTP.log", level=logging.INFO)
 win = ChFTP()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
